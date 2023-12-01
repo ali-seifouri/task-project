b3de5e36f8b51a6f6a75f198f3c63b421c037d18
Ali Seifouri <a.seifoori@emofid.com>
2023-12-01 16:57:24 +0330

    Put verb added

--- app/tasks_router.py
+++ app/tasks_router.py
@@ -11,8 +11,8 @@ from app.models import TaskModel
 router = APIRouter()
 
 
-@router.get("/")
-async def read_root():
+@router.get("/", description="Returns all tasks")
+async def find_all_tasks():
     from main import task_collection
     # noinspection PyGlobalUndefined
     global task_collection
@@ -20,8 +20,13 @@ async def read_root():
     return tasks
 
 
-@router.get("/tasks/")
-def read_item(request: Request):
+@router.get("/tasks/", description="Search tasks with field considered as string")
+def find_tasks_get(request: Request):
+    """
+    In order to search the items regarding the data types as string you should use this endpoint
+    :param request: Search criteria (they are all considered as string) are sent in query params
+    :return: Result
+    """
     params = jsonable_encoder(request.query_params)
     from main import task_collection
     # noinspection PyGlobalUndefined
@@ -30,26 +35,59 @@ def read_item(request: Request):
     return tasks
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/tasks/", description="Search tasks with field types considered")
+def find_tasks_post(request: Request, search_criteria=Body(...), ):
+    """
+    In order to search the items regarding the data types (type safe) you should use this endpoint
+    :param request:
+    :param search_criteria: Search Criteria (all user types are considered as entered)
+    :return: Result
+    """
+    from main import task_collection
+    # noinspection PyGlobalUndefined
+    global task_collection
+    tasks = [t for t in task_collection.find(search_criteria, {'_id': False})]
+    return tasks
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, description="Adds a task to database")
 async def add_task_data(
         request: Request,
         task=Body(...),
 ):
-        try:
-            TaskModel.model_validate(task)
-        except ValidationError as exc:
-            return Response(content='validation error', status_code=status.HTTP_400_BAD_REQUEST)
+    try:
+        TaskModel.model_validate(task)
+    except ValidationError as exc:
+        return Response(content='validation error', status_code=status.HTTP_400_BAD_REQUEST)
 
-        from main import task_collection
-        # noinspection PyGlobalUndefined
-        global task_collection
+    from main import task_collection
+    # noinspection PyGlobalUndefined
+    global task_collection
 
-        try:
-            t = task_collection.insert_one(task)
-            new_task = task_collection.find_one({"_id": t.inserted_id}, {'_id': False})
-            return new_task
-        except DuplicateKeyError as dke:
-            return dke.details.get("errmsg", "duplicate id")
-        except Exception as ex:
-            return ex.message
+    try:
+        t = task_collection.insert_one(task)
+        new_task = task_collection.find_one({"_id": t.inserted_id}, {'_id': False})
+        return new_task
+    except DuplicateKeyError as dke:
+        return dke.details.get("errmsg", "duplicate id")
+    except Exception as ex:
+        return ex.message
 
+
+@router.put("/{id}", description="Updates a task")
+async def update_task_data(id: int, req=Body(...)):
+    from main import task_collection
+    # noinspection PyGlobalUndefined
+    global task_collection
+
+    data = {k: v for k, v in req.items() if v is not None}
+    # updated_student = await update_student(id, req)
+    task = task_collection.find_one({"id": id})
+    if task:
+        updated_task = task_collection.update_one({"id": id}, {"$set": data})
+    if updated_task:
+        return Response(content=f"Task with ID: {id} update is successful")
+    return Response(
+        content="task not found",
+        status_code=status.HTTP_404_NOT_FOUND
+    )
